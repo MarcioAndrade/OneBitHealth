7f135d6a4aede63cb5a802bea1f3907c79c0e262
mancosta <mancosta@icatuseguros.com.br>
2021-12-31 12:24:07 -0300

    Calculo de IMC;

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, Button } from "react-native";
 
 import ResultImc from './ResultImc'
 
 export default function Form() {
+
+    const [height, setHeight] = useState(null);
+    const [weight, setWeight] = useState(null);
+    const [messageImc, setMessageImc] = useState("Preencha o peso e altura");
+    const [imc, setImc] = useState(null);
+    const [textButton, setTextButton] = useState("Calcular");
+
+    function imcCalculator() {
+        return setImc((weight / (height + height)).toFixed(2));
+    }
+
+    function validationImc() {
+        if (weight != null && height != null) {
+            imcCalculator();
+            setHeight(null);
+            setWeight(null);
+            setMessageImc("IMC: ");
+            setTextButton("Calcular novamente");
+        }
+        else {
+            setImc(null);
+            setTextButton("Calcular");
+            setImc("Preencha o peso e altura");
+        }
+    }
+
     return (
         <View>
             <View>
@@ -21,8 +47,7 @@ export default function Form() {
                     title="Calcular IMC"
                 />
             </View>
-            {/* <ResultImc messageResultImc={messageImc} resultImc={imc} /> */}
-            <ResultImc messageResultImc="{messageImc}" resultImc="{imc}" />
+            <ResultImc messageResultImc={messageImc} resultImc={imc} />
         </View>
     );
 }
